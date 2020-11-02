9565aeb548b684d1c149881794bbb9eabbc47ced
polko155 <kevin.gino.smm@gmail.com>
2020-11-02 10:27:31 -0300

    HTML general

---
+++ database/database.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE tienda_master;
+USE tienda_master;
+
+CREATE TABLE usuarios(
+id              int(255) auto_increment not null,
+nombre          varchar(100) not null,
+apellidos       varchar(255),
+email           varchar(255) not null,
+password        varchar(255) not null,
+rol             varchar(20),
+imagen          varchar(255),
+CONSTRAINT pk_usuarios PRIMARY KEY(id),
+CONSTRAINT uq_email UNIQUE(email)  
+)ENGINE=InnoDb;
+
+INSERT INTO usuarios VALUES(NULL, 'Admin', 'Admin', 'admin@admin.com', 'contraseña', 'admin', null);
+
+CREATE TABLE categorias(
+id              int(255) auto_increment not null,
+nombre          varchar(100) not null,
+CONSTRAINT pk_categorias PRIMARY KEY(id) 
+)ENGINE=InnoDb;
+
+INSERT INTO categorias VALUES(null, 'Manga corta');
+INSERT INTO categorias VALUES(null, 'Tirantes');
+INSERT INTO categorias VALUES(null, 'Manga larga');
+INSERT INTO categorias VALUES(null, 'Sudaderas');
+
+CREATE TABLE productos(
+id              int(255) auto_increment not null,
+categoria_id    int(255) not null,
+nombre          varchar(100) not null,
+descripcion     text,
+precio          float(100,2) not null,
+stock           int(255) not null,
+oferta          varchar(2),
+fecha           date not null,
+imagen          varchar(255),
+CONSTRAINT pk_categorias PRIMARY KEY(id),
+CONSTRAINT fk_producto_categoria FOREIGN KEY(categoria_id) REFERENCES categorias(id)
+)ENGINE=InnoDb;
+
