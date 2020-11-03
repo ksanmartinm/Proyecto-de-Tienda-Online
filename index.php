0d32bb3fedb6487556859d18ded3476e1942d867
polko155 <kevin.gino.smm@gmail.com>
2020-11-03 10:51:33 -0300

    avance css

--- index.php
+++ index.php
@@ -1,77 +1,91 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de Camisetas</title>
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
+
 <body>
-    <!-- CABECERA -->
-    <header id="header">
-    <div id="logo">
-    <img src="assets/img/camiseta.png" alt="Camiseta Logo">
-    <a href="index.php">
-        Tienda de Camisetas
-    </a>
-    </div>
-    </header>
-    <!-- MENU -->
-    <nav id="menu">
-        <ul>
-            <li>
-                <a href="#">Inicio</a>
-            </li>
-            <li>
-                <a href="#">Categoria 1</a>
-            </li>
-            <li>
-                <a href="#">Categoria 2</a>
-            </li>
-            <li>
-                <a href="#">Categoria 3</a>
-            </li>
-            <li>
-                <a href="#">Categoria 4</a>
-            </li>
-            <li>
-                <a href="#">Categoria 5</a>
-            </li>
-        </ul>    
-    </nav>
+    <div id="container">
+        <!-- CABECERA -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+                <a href="index.php">
+                    Tienda de Camisetas
+                </a>
+            </div>
+        </header>
+        <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
 
-    <div id="id">
-    
-    
-    <!-- BARRA LATERAL -->
-    <aside id="lateral">
-        <div id="login" class="block_aside">
-            <form action="#" method="post">
-                <label for="email">Email</label>
-                <input type="email" name="email">
-                <label for="password" name="password">Password</label>
-                <input type="password" name="password">
-                <input type="submit" value="Enviar">
-            </form>
+        <div id="id">
 
-            <a href="#">Mis pedidos</a>
-            <a href="#">Gestionar pedidos</a>
-            <a href="#">Gestionar categorias</a>
+
+            <!-- BARRA LATERAL -->
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <h3>Entrar a la web</h3>
+                    <form action="#" method="post">
+                        <label for="email">Email</label>
+                        <input type="email" name="email">
+                        <label for="password" name="password">Password</label>
+                        <input type="password" name="password">
+                        <input type="submit" value="Enviar">
+                    </form>
+                    <ul>
+                    <li>
+                        <a href="#">Mis pedidos</a>
+                    </li>
+                    <li>
+                        <a href="#">Gestionar pedidos</a>
+                    </li>
+                    <li>
+                        <a href="#">Gestionar categorias</a>
+                    </li>
+                    </ul>
+                    
+                </div>
+            </aside>
+            <!-- CONTENIDO CENTRAL -->
+            <div id="central">
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="Camiseta">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="">Comprar</a>
+                </div>
+            </div>
         </div>
-    </aside>
-    <!-- CONTENIDO CENTRAL -->
-    <div id="central">
-        <div class="product">
-            <img src="assets/img/camiseta.png" alt="Camiseta">
-            <h2>Camiseta Azul Ancha</h2>
-            <p>30 euros</p>
-            <a href="">Comprar</a>
-        </div>
+        <!-- PIE DE PAGINA -->
+        <footer id="footer">
+            <p>Desarrollado por Kevin San Martin Mundaca &copy; <?=date('Y') ?></p>
+        </footer>
     </div>
-    </div>
-    <!-- PIE DE PAGINA -->
-    <footer id="footer">
-        <p>Desarrollado por Kevin San Martin Mundaca &copy; <?=date('Y') ?></p>
-    </footer>
 </body>
+
 </html>
