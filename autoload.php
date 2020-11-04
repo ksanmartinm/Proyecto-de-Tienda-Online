bcc96eee7bcedd70aff0eebeaa3a42851db22ec0
unknown <kevin.gino.smm@gmail.com>
2020-11-04 19:01:19 -0300

    estructura mvc

---
+++ autoload.php
@@ -0,0 +1,7 @@
+<?php
+function autocargar($classname){
+    include 'controller/'. $classname .'.php';
+}
+
+
+?>
--- index_plantilla.php
+++ index_plantilla.php
@@ -73,11 +73,24 @@
             </aside>
             <!-- CONTENIDO CENTRAL -->
             <div id="central">
+            <h1>Productos Destacados</h1>
                 <div class="product">
                     <img src="assets/img/camiseta.png" alt="Camiseta">
                     <h2>Camiseta Azul Ancha</h2>
                     <p>30 euros</p>
-                    <a href="">Comprar</a>
+                    <a href="" class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="Camiseta">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="Camiseta">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button">Comprar</a>
                 </div>
             </div>
         </div>
